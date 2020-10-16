6ae77db87948692e8c45fe76f7fe3bc21d3070b0
mrl@ualberta.ca <michaellong@Michaels-Mac-mini.local>
2020-10-16 01:19:58 -0600

    Created design templates

---
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/templates/BlankTemplateActivity.java
@@ -0,0 +1,32 @@
+package com.example.bobslittlefreelibrary.templates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.bobslittlefreelibrary.MainActivity;
+import com.example.bobslittlefreelibrary.R;
+
+public class BlankTemplateActivity extends AppCompatActivity {
+
+    private Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank_template);
+
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BlankTemplateActivity.this, MainActivity.class);
+                BlankTemplateActivity.this.startActivity(intent);
+            }
+        });
+    }
+}

509902422e6566223bfd03500b97debf2609122d
Khang <khang.nguyen5700@gmail.com>
2020-10-21 22:49:16 -0600

    Small code cleanup
    Added some comments too.

--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/RequestsFragment.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/RequestsFragment.java
@@ -9,11 +9,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class RequestsFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.requests_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Create the top tabs when the fragment is created
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
     }
 }

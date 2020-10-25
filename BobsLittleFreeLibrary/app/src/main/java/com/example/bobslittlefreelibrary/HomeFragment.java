c3b77763efce9d6ec972b52829021c8c36b3805f
Khang <khang.nguyen5700@gmail.com>
2020-10-25 01:51:34 -0600

    Updated home_fragment.xml
    + Added UI elements to home_fragment.xml
        - Button for SearchActivity
        - Button for UserProfileViewActivity
        - Table view for Latest Books (Shows the last 6 books to be added to the database)
        - Table view for Requests Overview (shows how many requests a user currently has)
        - Button for ExchangeBookActivity (aka scan activity)
    + Added onCreate(), onActivityCreate(), and onPause() to HomeFragment.java
    + Commented out the buttons that lead to temp assets

--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/HomeFragment.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +12,29 @@ import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Setup references to UI elements
+        Button searchButton = getView().findViewById(R.id.home_search_button); // getView() cannot be called in onCreate() since the view isn't inflated yet (onCreate --> onCreateView() --> onActivityCreated()
+    }
+
+    @Override
+    public void onPause() {
+        // Is called when the user switches away from the fragment
+        super.onPause();
     }
 }
--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/MainActivity.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        formTemplateButton = findViewById(R.id.form_template);
+        /*formTemplateButton = findViewById(R.id.form_template);
         blankTemplateButton = findViewById(R.id.blank_template);
         someAssetsButton = findViewById(R.id.some_assets);
 
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SomeAssetsActivity.class);
                 MainActivity.this.startActivity(intent);
             }
-        });
+        });*/
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -88,3 +88,31 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
+
+/* code for extra template buttons
+*     <Button
+        android:id="@+id/form_template"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Form Template"
+        style="@style/SubmitButton"
+        android:layout_marginTop="100dp"
+        android:layout_marginHorizontal="11dp"/>
+    <Button
+        android:id="@+id/blank_template"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Blank Template"
+        android:layout_below="@+id/form_template"
+        style="@style/SubmitButton"
+        android:layout_margin="11dp"/>
+    <Button
+        android:id="@+id/some_assets"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Some Assets"
+        android:layout_below="@+id/blank_template"
+        style="@style/SubmitButton"
+        android:layout_margin="11dp"/>*/

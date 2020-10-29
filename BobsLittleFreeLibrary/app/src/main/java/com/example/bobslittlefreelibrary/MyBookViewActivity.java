7b9fc96da7a79d5da8b18fcb6ffa73ea8d36587d
Khang <khang.nguyen5700@gmail.com>
2020-10-29 14:59:51 -0600

    Began work on setup for Book View Activities
    + Added class variables for the Book and UI elements
    + Created setupUIReferences() which has all of the findViewById() calls
    + Created SetUIValues() which takes in a book object and then sets the values of the UI elements using the book's getters.
    + Setup the onclicklisteners for the buttons
    
    + Added some more strings to strings.xml
    + Added both book view activities to the manifest
    
    + Setup button functionality
    + initialize imageView
    + populate profile button with username

--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/MyBookViewActivity.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/MyBookViewActivity.java
@@ -1,20 +1,98 @@
 package com.example.bobslittlefreelibrary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * This activity provides a location to display all the information that pertains to a Book owned by the User
- * TODO:
+ * TODO: Setup button functionality, initialize imageView, populate profile button with username
  *
  * */
 public class MyBookViewActivity extends AppCompatActivity {
+
+    private Book book;
+    private ImageView bookImage;
+    private TextView titleText;
+    private TextView authorText;
+    private TextView ISBNText;
+    private TextView descText;
+    private Button borrowerProfileButton;
+    private TextView bookStatus;
+    private Button removeBookButton;
+    private Button editInfoButton;
+    private Button backButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_book_view);
+
+        // Get Book object passed from Intent
+        Intent intent = getIntent();
+        //book = intent.getSerializableExtra("BOOK"); // need to make Book class serializable
+        //Book book = new Book("Test Book", "available", "this is a test description", "Khang", );
+
+        // Set references to UI elements
+        setupUIReferences();
+        // Set UI values
+        //setUIValues(book);
+
+        // Set onClickListeners for the buttons
+        borrowerProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TEMP", "Borrower Profile button pressed");
+            }
+        });
+        removeBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TEMP", "Remove Book button pressed");
+            }
+        });
+        editInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TEMP", "Edit Info button pressed");
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TEMP", "Back button pressed");
+            }
+        });
+    }
+
+    private void setupUIReferences() {
+        bookImage = findViewById(R.id.my_book_view_image);
+        titleText = findViewById(R.id.my_book_view_title);
+        authorText = findViewById(R.id.my_book_view_author);
+        ISBNText = findViewById(R.id.my_book_view_ISBN);
+        descText =findViewById(R.id.my_book_view_desc);
+        borrowerProfileButton = findViewById(R.id.my_book_view_borrower_profile_button);
+        bookStatus = findViewById(R.id.my_book_view_status_text);
+        removeBookButton = findViewById(R.id.my_book_view_remove_button);
+        editInfoButton = findViewById(R.id.my_book_view_edit_button);
+        backButton = findViewById(R.id.my_book_view_back_button);
+
+    }
+
+    private void setUIValues(Book book) {
+        // bookImage.setImageResource("@drawable/" + book.getPicture());
+        titleText.setText(book.getTitle());
+        authorText.setText(book.getAuthor());
+        ISBNText.setText(book.getISBN());
+        descText.setText(book.getDescription());
+        // borrowerProfileButton
+        bookStatus.setText(book.getStatus());
     }
 }
--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/PublicBookViewActivity.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/PublicBookViewActivity.java
@@ -1,20 +1,89 @@
 package com.example.bobslittlefreelibrary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * This activity provides a location to display all the information that pertains to a Book owned by another User
- * TODO:
+ * TODO: Setup button functionality, initialize imageView, populate profile button with username
  *
  * */
 public class PublicBookViewActivity extends AppCompatActivity {
+
+    private Book book;
+    private ImageView bookImage;
+    private TextView titleText;
+    private TextView authorText;
+    private TextView ISBNText;
+    private TextView descText;
+    private Button ownerProfileButton;
+    private TextView bookStatus;
+    private Button requestButton;
+    private Button backButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_book_view);
+
+        // Get Book object passed from Intent
+        Intent intent = getIntent();
+        //book = intent.getSerializableExtra("BOOK"); // need to make Book class serializable
+
+        // Set references to UI elements
+        setupUIReferences();
+        // Set UI values
+        //setUIValues(book);
+
+        // Set onClickListeners for the buttons
+        ownerProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TEMP", "Owner Profile button pressed");
+            }
+        });
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TEMP", "Request Book button pressed");
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TEMP", "Back button pressed");
+            }
+        });
+    }
+
+    private void setupUIReferences() {
+        bookImage = findViewById(R.id.public_book_view_image);
+        titleText = findViewById(R.id.public_book_view_title);
+        authorText = findViewById(R.id.public_book_view_author);
+        ISBNText = findViewById(R.id.public_book_view_ISBN);
+        descText =findViewById(R.id.public_book_view_desc);
+        ownerProfileButton = findViewById(R.id.public_book_view_owner_profile_button);
+        bookStatus = findViewById(R.id.public_book_view_status_text);
+        requestButton = findViewById(R.id.public_book_view_request_button);
+        backButton = findViewById(R.id.public_book_view_back_button);
+
+    }
+
+    private void setUIValues(Book book) {
+        // bookImage.setImageResource("@drawable/" + book.getPicture());
+        titleText.setText(book.getTitle());
+        authorText.setText(book.getAuthor());
+        ISBNText.setText(book.getISBN());
+        descText.setText(book.getDescription());
+        // ownerProfileButton // probably get book.getOwner() and then either populate button with that User object or do smthn with db
+        bookStatus.setText(book.getStatus());
     }
 }

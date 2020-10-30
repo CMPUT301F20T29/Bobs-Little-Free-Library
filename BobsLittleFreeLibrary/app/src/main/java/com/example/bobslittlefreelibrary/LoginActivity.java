528302aa308b75ed04004188f524ec443b674b85
Maristella <hyunseo@ualberta.ca>
2020-10-30 12:45:38 -0600

    added signup and login layout and activities

--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/LoginActivity.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/LoginActivity.java
@@ -1,4 +1,27 @@
 package com.example.bobslittlefreelibrary;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bobslittlefreelibrary.templates.FormTemplateActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button signupButton = findViewById(R.id.signUpBtn);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }

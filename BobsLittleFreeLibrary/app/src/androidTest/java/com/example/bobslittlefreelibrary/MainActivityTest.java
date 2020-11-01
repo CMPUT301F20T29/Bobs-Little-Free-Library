04ba72f34c16f9c60bc90fd20c01689304f5a647
Khang <khang.nguyen5700@gmail.com>
2020-10-31 23:53:00 -0600

    Added Robotium dependencies & created MainActivityTest.java

---
+++ BobsLittleFreeLibrary/app/src/androidTest/java/com/example/bobslittlefreelibrary/MainActivityTest.java
@@ -0,0 +1,72 @@
+package com.example.bobslittlefreelibrary;
+
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This is a test class that tests the functionality and UI of the 3 main tab (fragments) shown in MainActivity.
+ * The tests use Robotium and ActivityTestRule (which is depreciated). Maybe consider using ActivityScenarioRule and see if robotium works with it?
+ *
+ * TODO: Make tests for: switching between fragments (Books, Home, Requests),
+ *
+ *
+ * */
+
+public class MainActivityTest {
+
+    private Solo solo;
+
+    /**
+     *
+     * */
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     *
+     * */
+    @Before
+    public void setup() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        // change it so that we start at LoginActivity, and then sign in, and then get to MainActivity where we begin the tests
+    }
+
+    /**
+     *
+     * */
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    /**
+     *
+     * */
+    @Test
+    public void checkStartHomeFragment() {
+        solo.assertCurrentActivity("Started on the wrong Activity", MainActivity.class);
+        assertTrue(solo.searchText("Latest Books"));
+    }
+
+    /**
+     *
+     * */
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+
+}

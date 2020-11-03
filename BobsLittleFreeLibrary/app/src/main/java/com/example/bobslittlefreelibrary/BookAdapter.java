65a20819839f1fd1c6bbed2a274c24bde3263c1d
Tij Sachdeva <ks.sachdeva05@gmail.com>
2020-11-02 22:49:53 -0700

    Created Layout and Adapter for Search List
    Still need to implement it in Search Activity and connect that activity to the Home Fragment

---
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/BookAdapter.java
@@ -0,0 +1,41 @@
+package com.example.bobslittlefreelibrary;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BookAdapter extends ArrayAdapter<Book> {
+
+    public BookAdapter(Context context, int resource, List<Book> bookSearchList) {
+        super(context, resource, bookSearchList);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Book book = getItem(position);
+
+        // Initializing the book cell view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.book_cell, parent, false);
+        }
+
+        TextView tv = (TextView) convertView.findViewById(R.id.bookSearchName);
+        ImageView iv = (ImageView) convertView.findViewById(R.id.bookSearchImage);
+
+        tv.setText(book.getTitle());
+        //iv.setImageResource();
+
+        return convertView;
+    }
+}

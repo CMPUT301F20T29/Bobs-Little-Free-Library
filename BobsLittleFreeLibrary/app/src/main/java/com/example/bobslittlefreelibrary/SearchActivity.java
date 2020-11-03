a0cb64c3030244f208e001e3f6343a37706e661e
Tij Sachdeva <ks.sachdeva05@gmail.com>
2020-11-02 23:38:34 -0700

    Updated Search Activity
    Yet to complete to with respect to recent updates

--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/SearchActivity.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/SearchActivity.java
@@ -3,12 +3,29 @@ package com.example.bobslittlefreelibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
+    // setting up the ArrayList
+    public static ArrayList<Book> searchBookList = new ArrayList<Book>();
+
+    // making a reference to the listView
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    private void setupData() {
+
+    }
+
+    private void setupList() {
+
     }
 }

7172c30795da4f7d3a088981993e13c44bc9872d
Tij Sachdeva <ks.sachdeva05@gmail.com>
2020-11-03 16:24:05 -0700

    Search Activity Test Implementation
    Activity working. Need to implement search bar and filters now.

--- BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/SearchActivity.java
+++ BobsLittleFreeLibrary/app/src/main/java/com/example/bobslittlefreelibrary/SearchActivity.java
@@ -19,15 +19,37 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        setupData();
+        setupList();
     }
 
 
     private void setupData() {
 
+        Book book1 = new Book("Test Title 1", "Test Author 1", "1-56619-909-3", "Nice", "123", "available");
+        searchBookList.add(book1);
+
+        Book book2 = new Book("Test Title 2", "Test Author 2", "1-56619-909-4", "Cool", "234", "available");
+        searchBookList.add(book2);
+
+        Book book3 = new Book("Test Title 3", "Test Author 3", "1-56619-909-5", "Wow", "345", "available");
+        searchBookList.add(book3);
+
+
+
+
+
     }
 
 
     private void setupList() {
+
+        listView = (ListView) findViewById(R.id.bookSearchListView);
+
+        // make reference to the BookAdapter that we created
+        BookAdapter adapter = new BookAdapter(getApplicationContext(), 0, searchBookList);
+        listView.setAdapter(adapter);
 
     }
 }
